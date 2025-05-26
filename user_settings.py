2d30e9016ef2ce132f705cbaa05baab098976832
Eduard Jereghi <ntorque170@gmail.com>
2025-05-26 15:00:30 +0300

    Added method to edit config file so the library change remains after restart

--- user_settings.py
+++ user_settings.py
@@ -7,6 +7,7 @@ class UserSettings:
 
     at_cli = 0  # toggle to 1 to use getpass when at cli for password entry
     use_sqlite3 = 0  # toggle to 1 to use a local sqlite3 file as a database
+    user_library_name = ""  # set name of the in use book library
 
     @staticmethod
     def set_config():
@@ -14,6 +15,8 @@ class UserSettings:
             user_settings = UserSettings.read_config("config.ini", "USER-SETTINGS")
             UserSettings.at_cli = int(user_settings["at_cli"])
             UserSettings.use_sqlite3 = int(user_settings["use_sqlite3"])
+            user_settings = UserSettings.read_config("config.ini", "USER-LIBRARY")
+            UserSettings.user_library_name = user_settings["name"]
         except ValueError:
             print("Invalid config file")
             exit()
@@ -35,6 +38,21 @@ class UserSettings:
         # get config options
         values = config_object[settings]
         return values
+
+    @staticmethod
+    def edit_config(file_name, settings, key, value):
+        # read the file
+        config_object = ConfigParser()
+        if not os.path.exists(file_name):
+            raise FileNotFoundError
+        config_object.read(file_name)
+
+        # edit the value
+        config_object.set(settings, key, value)
+
+        # write the file
+        with open(file_name, "w") as conf:
+            config_object.write(conf)
 
     @staticmethod
     def read_menu_option(prompt):
